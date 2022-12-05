346c8a835d064e09566b13ddce4b864ff1b0e362
orhunrustuoglu <orhun.rustuoglu@gmail.com>
2022-12-05 03:19:18 +0300

    Unit Test w/bloc_test performed

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,12 +1,17 @@
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:etiya_test_app/bloc/user_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:bloc_test/bloc_test.dart';
 
-// void main() {
-//   test("unit testing", () {
-//     //setup
-
-//     //do
-
-//     //test
-
-//   });
-// }
+void main() {
+  // commented out the auto_route_generator package in the pubspec.yaml
+  //because its incompatible with the bloc_test package
+  //which is needed for unit testing
+  group('UserBloc', () {
+    blocTest(
+      'emits UserLoaded state when FetchUsers event is added',
+      build: () => UserBloc()..add(FetchUsers()),
+      wait: const Duration(seconds: 2),
+      expect: () => [isA<UserLoaded>()],
+    );
+  });
+}
